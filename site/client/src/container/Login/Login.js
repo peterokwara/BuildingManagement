cdb76ba31eb2dd885c9a481325d05bf891e67dbe
peterokwara <peter.okwara@outlook.com>
2019-01-26 02:51:19 +0300

    Add login page and login logic

--- site/client/src/container/Login/Login.js
+++ site/client/src/container/Login/Login.js
@@ -1,8 +1,31 @@
 import React, { Component } from "react";
 import { Button, Form, Grid, Header, Message } from "semantic-ui-react";
+import { Link, Redirect } from "react-router-dom";
 import "./Login.css";
+import client from "../../feathers";
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  updateField(name, ev) {
+    this.setState({ [name]: ev.target.value });
+  }
+
+  login() {
+    const { email, password } = this.state;
+
+    return client
+      .authenticate({
+        strategy: "local",
+        email,
+        password
+      })
+      .catch(error => this.setState({ error }));
+  }
+
   render() {
     return (
       <div className="login-form">
@@ -13,19 +36,25 @@ class Login extends Component {
             </Header>
             <Form>
               <Form.Field>
-                <label>Username</label>
-                <input size="mini" placeholder="Username" />
+                <label>email</label>
+                <input
+                  placeholder="Username"
+                  onChange={ev => this.updateField("email", ev)}
+                />
               </Form.Field>
               <Form.Field>
                 <label>Password</label>
-                <input placeholder="Password" />
+                <input
+                  placeholder="Password"
+                  onChange={ev => this.updateField("password", ev)}
+                />
               </Form.Field>
-              <Button type="submit" fluid>
+              <Button type="submit" fluid onClick={() => this.login()}>
                 Login
               </Button>
             </Form>
-            <Message size="tiny">
-              New to us? <a href="#">Sign Up</a>
+            <Message>
+              Don't have an account? <Link to="/signup">Sign Up</Link>
             </Message>
           </Grid.Column>
         </Grid>
