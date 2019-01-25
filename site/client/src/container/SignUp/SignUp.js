5709c6fa49732f6c002519a105de31956e120d08
peterokwara <peter.okwara@outlook.com>
2019-01-26 02:51:38 +0300

    Add signup page and signup logic

--- site/client/src/container/SignUp/SignUp.js
+++ site/client/src/container/SignUp/SignUp.js
@@ -1,8 +1,27 @@
 import React, { Component } from "react";
 import { Button, Checkbox, Form, Grid, Header } from "semantic-ui-react";
 import "./SignUp.css";
+import client from "../../feathers";
 
 class SignUp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  signup() {
+    const { email, password } = this.state;
+
+    return client
+      .service("users")
+      .create({ email, password })
+      .then(this.props.history.push("/"));
+  }
+
+  updateField(name, ev) {
+    this.setState({ [name]: ev.target.value });
+  }
+
   render() {
     return (
       <div className="signUp-form">
@@ -13,14 +32,25 @@ class SignUp extends Component {
             </Header>
             <Form fluid>
               <Form.Field>
-                <label>Username</label>
-                <input placeholder="Username" />
+                <label>email</label>
+                <input
+                  placeholder="Username"
+                  onChange={ev => this.updateField("email", ev)}
+                />
               </Form.Field>
               <Form.Field>
                 <label>Password</label>
-                <input placeholder="Password" />
+                <input
+                  placeholder="Password"
+                  onChange={ev => this.updateField("password", ev)}
+                />
               </Form.Field>
-              <Button type="submit" fluid size="large">
+              <Button
+                type="submit"
+                fluid
+                size="large"
+                onClick={() => this.signup()}
+              >
                 Sign Up
               </Button>
             </Form>
