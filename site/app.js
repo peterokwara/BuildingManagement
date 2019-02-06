ab3545a79234a330705f922f4907c9d456589a34
peterokwara <peter.okwara@outlook.com>
2019-02-06 20:23:05 +0300

    Connect to remote mongodb database

--- site/app.js
+++ site/app.js
@@ -1,5 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+
+// db configuration
+const db = require("./config/config").mongoUri;
+
+// connect to mongodb
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
 const port = 3000;
 
 app.get("/", (req, res) => res.send("Hello World!"));
