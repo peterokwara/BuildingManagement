41ac0ccfa174756a6ebef97bb37e560294be85d0
peterokwara <peter.okwara@outlook.com>
2019-02-06 22:59:51 +0300

    Store user data posted

--- site/routes/api/users.js
+++ site/routes/api/users.js
@@ -1,9 +1,41 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+
+// load user model
+const User = require("../../models/user");
 
 // @route   GET api/users/test
 // @desc    Tests users route
 // @access  Public
 router.get("/test", (req, res) => res.json({ msg: "Users Works" }));
+
+// @route   GET api/users/register
+// @desc    Register user
+// @access  Public
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "Email already exists" });
+    } else {
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(console.log(err));
+        });
+      });
+    }
+  });
+});
 
 module.exports = router;
