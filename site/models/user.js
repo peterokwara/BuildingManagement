8cc65f58faf99d4bfb26b7a3aacf77ca06904cf5
peterokwara <peter.okwara@outlook.com>
2019-02-06 23:17:11 +0300

    Add user avatar

--- site/models/user.js
+++ site/models/user.js
@@ -15,6 +15,9 @@ const UserSchema = new Schema({
     type: String,
     required: true
   },
+  avatar: {
+    type: String
+  },
   date: {
     type: Date,
     default: Date.now
--- site/routes/api/users.js
+++ site/routes/api/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
+const gravatar = require("gravatar");
 
 // load user model
 const User = require("../../models/user");
@@ -18,9 +19,15 @@ router.post("/register", (req, res) => {
     if (user) {
       return res.status(400).json({ email: "Email already exists" });
     } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: "200", // size
+        r: "pg", // rating
+        d: "mm" // default
+      });
       const newUser = new User({
         name: req.body.name,
         email: req.body.email,
+        avatar,
         password: req.body.password
       });
 
