7c01cdbb9cf7d902da2910184efde03c3703cd0c
peterokwara <peter.okwara@outlook.com>
2019-02-07 01:38:37 +0300

    Print message whether user authenticated successfully or not

--- site/routes/api/users.js
+++ site/routes/api/users.js
@@ -45,4 +45,29 @@ router.post("/register", (req, res) => {
   });
 });
 
+// @route   GET api/users/login
+// @desc    Login user and return user JWT token
+// @access  Public
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // find the user by email
+  User.findOne({ email }).then(user => {
+    // check for user
+    if (!user) {
+      return res.status(404).json({ email: "User not found" });
+    }
+
+    // check password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        res.json({ msg: "Password success" });
+      } else {
+        return res.status(400).json({ password: "Password incorrect" });
+      }
+    });
+  });
+});
+
 module.exports = router;
