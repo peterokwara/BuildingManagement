418812aeb3aa049701eb219fc37de352f71b26eb
peterokwara <peter.okwara@outlook.com>
2019-02-10 01:03:16 +0300

    Add actions for logging out

--- site/client/src/actions/authActions.js
+++ site/client/src/actions/authActions.js
@@ -47,3 +47,13 @@ export const setCurrentUser = decoded => {
     payload: decoded
   };
 };
+
+// log user out
+export const logoutUser = () => dispatch => {
+  // remove token from local storage
+  localStorage.removeItem("jwtToken");
+  // remove auth header for future requests
+  setAuthToken(false);
+  // set current user to {} which will set isAuthenticated to false
+  dispatch(setCurrentUser({}));
+};
