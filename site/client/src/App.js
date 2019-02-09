152e8a31ff577844164e30c3ea6cd489d0dab762
peterokwara <peter.okwara@outlook.com>
2019-02-10 01:16:26 +0300

    Complete redirect to logout

--- site/client/src/App.js
+++ site/client/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import SignIn from "./container/SignIn/SignIn";
 import SignUp from "./container/SignUp/SignUp";
+import MainPage from "./container/MainPage/MainPage";
 import { Provider } from "react-redux";
 import store from "./store";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
-import { setCurrentUser } from "./actions/authActions";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
+import PrivateRoute from "./components/Common/PrivateRoute";
 
 // check for token
 if (localStorage.jwtToken) {
@@ -17,6 +19,15 @@ if (localStorage.jwtToken) {
   const decoded = jwt_decode(localStorage.jwtToken);
   // set user and isAuthenticated
   store.dispatch(setCurrentUser(decoded));
+  // check for expired token
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    // logout the user
+    store.dispatch(logoutUser);
+    // TODO: clear current profile
+    // Redirect to login
+    window.location.href = "/";
+  }
 }
 
 class App extends Component {
@@ -27,6 +38,9 @@ class App extends Component {
           <div className="App">
             <Route exact path="/" component={SignIn} />
             <Route exact path="/signup" component={SignUp} />
+            <Switch>
+              <PrivateRoute exact path="/mainpage" component={MainPage} />
+            </Switch>
           </div>
         </Router>
       </Provider>
