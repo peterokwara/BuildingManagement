f3426a14748c738864cd1ec6fc908435ed1ff5ea
peterokwara <peter.okwara@outlook.com>
2019-02-10 01:02:42 +0300

    Create a top menu

---
+++ site/client/src/container/MainPage/MainPage.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import TopMenu from "../../components/TopMenu/TopMenu";
+
+class MainPage extends Component {
+  render() {
+    return <TopMenu />;
+  }
+}
+
+export default MainPage;
