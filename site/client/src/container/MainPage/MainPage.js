11e3ef710d9ca46d316c538dec8168e18a47c71e
peterokwara <peter.okwara@outlook.com>
2019-02-10 17:06:27 +0300

    Add basic layout

--- site/client/src/container/MainPage/MainPage.js
+++ site/client/src/container/MainPage/MainPage.js
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
 import TopMenu from "../../components/TopMenu/TopMenu";
 import LeftMenu from "../../components/LeftMenu/LeftMenu";
+import MotionSensor from "../../components/MotionSensor/MotionSensor";
+import { Grid } from "semantic-ui-react";
 
 class MainPage extends Component {
   render() {
     return (
       <div>
         <TopMenu />
-        <LeftMenu />
+        <Grid>
+          <Grid.Column width={2}>
+            <LeftMenu />
+          </Grid.Column>
+          <Grid.Column width={14}>
+            <MotionSensor />
+          </Grid.Column>
+        </Grid>
       </div>
     );
   }
