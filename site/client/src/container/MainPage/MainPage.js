91932770a8514b3ca1207bdecd6645570db927a2
peterokwara <peter.okwara@outlook.com>
2019-02-10 15:51:02 +0300

    Add left menu to mainpage

--- site/client/src/container/MainPage/MainPage.js
+++ site/client/src/container/MainPage/MainPage.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
 import TopMenu from "../../components/TopMenu/TopMenu";
+import LeftMenu from "../../components/LeftMenu/LeftMenu";
 
 class MainPage extends Component {
   render() {
-    return <TopMenu />;
+    return (
+      <div>
+        <TopMenu />
+        <LeftMenu />
+      </div>
+    );
   }
 }
 
