98bfee7a03135de74a5cde70a04bb934ce301ec9
peterokwara <peter.okwara@outlook.com>
2019-05-08 05:17:10 +0300

    Add comments

--- esp/relay-bulb/main.py
+++ esp/relay-bulb/main.py
@@ -1,3 +1,8 @@
+#Peter Okwara
+# F17/36211/2013 
+
+# This piece of code awaits for instructions to turn on or off a GPIO pin to control a relay
+
 import connectWifi
 import time
 import config
@@ -6,7 +11,10 @@ from dht import DHT11
 from machine import Pin
 from umqtt.simple import MQTTClient
 
+# connect to wifi
 connectWifi.connect()
+
+# set GPIO_2 pin as an output pin
 RPin = Pin(2, Pin.OUT, value=0)
 
 SERVER = config.MQTT_CONFIG['MQTT_HOST']
@@ -16,21 +24,21 @@ PUB_TOPIC = config.MQTT_CONFIG['PUB_TOPIC']
 
 c = MQTTClient(SENSOR_ID, SERVER)
 
-
+# function to set relay on
 def relay_on():
     send("Relay on")
     RPin.on()
 
-
+# function to set relay off
 def relay_off():
     send("Relay off")
     RPin.off()
 
-
+# function to send confirmation that relay is on or off
 def send(data):
     c.publish(PUB_TOPIC, json.dumps(data))
 
-
+# function to subscribe for incomimg commands
 def sub_cb(topic, msg):
     print((topic, msg))
     command = msg.decode('ASCII')
@@ -44,6 +52,7 @@ def main(server=SERVER):
     c.set_callback(sub_cb)
     c.connect()
     c.subscribe(PUB_TOPIC)
+    # check for incoming messages/commands
     while True:
         if True:
             # Blocking wait for message
