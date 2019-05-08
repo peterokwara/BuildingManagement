86bc03345bbcbb7608aefbf40dfbb1406434e504
peterokwara <peter.okwara@outlook.com>
2019-05-08 04:50:20 +0300

    Add comments

--- esp/dht-sensor/main.py
+++ esp/dht-sensor/main.py
@@ -1,3 +1,9 @@
+#Peter Okwara
+# F17/36211/2013 
+
+# This piece of code collects temperature and humidity values from the DHT11 temperature
+# and humidity sensor and transmits them to the mqtt broker
+
 import connectWifi
 import time
 import config
@@ -6,7 +12,10 @@ from dht import DHT11
 from machine import Pin
 from umqtt.simple import MQTTClient
 
+# connect to wifi first
 connectWifi.connect()
+
+# set the pin for reading the humidity ant temperature values
 d = DHT11(Pin(2))
 
 SERVER = config.MQTT_CONFIG['MQTT_HOST']
@@ -14,7 +23,7 @@ PORT = config.MQTT_CONFIG['PORT']
 SENSOR_ID = config.MQTT_CONFIG['SENSOR_ID']
 PUB_TOPIC = config.MQTT_CONFIG['PUB_TOPIC']
 
-
+# function for reading the temperature and humidity values
 def read_sensor():
     d.measure()
     return {
@@ -22,7 +31,7 @@ def read_sensor():
         "humidity": d.humidity()
     }
 
-
+# function for sending the temperature and humidity values
 def send(data):
     c = MQTTClient(SENSOR_ID, SERVER, 1883)
     c.connect()
@@ -32,9 +41,12 @@ def send(data):
 
 def main():
     while True:
+        # get the values for temperature and humidity
         data = read_sensor()
         print("Sending data", data)
+        # transmit the values for temperature and humidity
         send(data)
+        # delay for 8 seconds before reading the values again
         time.sleep(8)
 
 
