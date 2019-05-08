1eac18804c3ea0e2d5b9151543367ba24f749e49
peterokwara <peter.okwara@outlook.com>
2019-05-08 08:37:01 +0300

    Add comments

--- raspberry-pi/app.js
+++ raspberry-pi/app.js
@@ -1,3 +1,10 @@
+// Peter Okwara
+// F17/36211/2013
+
+// This piece of code performs automation (if temperature is above 30 degrees turn on fan
+// else turn off fan) and (if occupancy is below 0 turn off bulb, if it is above 0 turn on
+// bulb) and also relays all incoming information to a database
+
 const mqtt = require("mqtt");
 const axios = require("axios");
 
@@ -24,14 +31,20 @@ client.on("connect", function() {
   client.subscribe("building/room/power");
   console.log("client has subscribed successfully");
 });
-// subscribe to power info
+// subscribe to relay for the fan
 client.on("connect", function() {
   client.subscribe("building/room/relay/fan");
   console.log("client has subscribed successfully");
 });
-// subscribe to power info
+// subscribe to relay for the bulb
 client.on("connect", function() {
   client.subscribe("building/room/relay/bulb");
+  console.log("client has subscribed successfully");
+});
+
+// subscribe to room occupancy
+client.on("connect", function() {
+  client.subscribe("building/room/occupancy");
   console.log("client has subscribed successfully");
 });
 
@@ -39,17 +52,18 @@ client.on("message", function(topic, message) {
   console.log("message is " + message.toString());
   console.log("topic is " + topic.toString());
   switch (topic.toString()) {
+    
+    // when we receive information on climate in the room
     case "building/room/climate":
-      console.log(JSON.parse(message).temperature);
+
+      // check if temperature is above 30, if it is turn on fan, if it is not turn off fan
       if (JSON.parse(message).temperature > 30) {
-        console.log("Temperature above 30");
-        console.log(JSON.parse(message).temperature);
         client.publish("building/room/relay/fan", "on");
       } else {
         client.publish("building/room/relay/fan", "off");
-        console.log("Temperature below 30");
-        console.log(JSON.parse(message).temperature);
       }
+
+      // format the climate data into a json format and post it to a database
       var data = {
         temperature: JSON.parse(message).temperature,
         humidity: JSON.parse(message).humidity,
@@ -61,7 +75,11 @@ client.on("message", function(topic, message) {
         .then(res => console.log(res))
         .catch(err => console.log(err));
       return;
+
+    // when we receive information about power in the room
     case "building/room/power":
+
+      // format the data into a json format and then post it to an online database
       var data = {
         power: JSON.parse(message).power,
         current: JSON.parse(message).current,
@@ -73,7 +91,19 @@ client.on("message", function(topic, message) {
         .then(res => console.log(res))
         .catch(err => console.log(err));
       return;
+
+    // when we receive information on room occupancy
     case "building/room/occupancy":
+
+      // if room occupancy is less than zero, turn off the bulb. if it is not turn on 
+      // the bulb
+      if (JSON.parse(message).occupancy <= 0) {
+        client.publish("building/room/relay/bulb", "off");
+      } else {
+        client.publish("building/room/relay/bulb", "on");
+      }
+
+      // put the occupancy data into a json format and post it to an online database
       var data = {
         occupancy: JSON.parse(message).occupancy,
         room_id: "1"
@@ -83,8 +113,12 @@ client.on("message", function(topic, message) {
         .then(res => console.log(res))
         .catch(err => console.log(err));
       return;
+    // when we receive information about the fan
     case "building/room/relay/fan":
       let fan_state = message;
+
+      // if fan state is on, publish on the database that the fan state is on
+      // if fan state is off, publish on the database that the fan state is off
       if (fan_state == "on") {
         fan = "1";
         var data = {
@@ -109,8 +143,12 @@ client.on("message", function(topic, message) {
           .catch(err => console.log(err));
       }
       return;
+    // when we receive information about the bulb
     case "building/room/relay/bulb":
       let bulb_state = message;
+
+      // if bulb state is on, publish on the database that the fan state is on
+      // if bulb state is off, publish on the database that the fan state is off
       if (bulb_state == "on") {
         bulb = "1";
         var data = {
@@ -135,23 +173,6 @@ client.on("message", function(topic, message) {
           .catch(err => console.log(err));
       }
       return;
-    default:
-      break;
-  }
-  switch (message.toString()) {
-    case "pir1on":
-      var data = { sensor1: 1, sensor2: 0 };
-      axios
-        .post(server + "/api/pir", data)
-        .then(res => console.log(res))
-        .catch(err => console.log(err));
-      console.log(server + "/api/pir");
-    case "pir2on":
-      var data = { sensor1: 0, sensor2: 1 };
-      axios
-        .post(server + "/api/pir", data)
-        .then(res => console.log(res))
-        .catch(err => console.log(err));
     default:
       break;
   }
